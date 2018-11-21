1f70f71dfb7cbbcd8a6222581290cfc86b324c6c
aRtoo <raphaelg2012@gmail.com>
2018-11-21 09:11:58 -0800

    create api folder for backend data

---
+++ api/controllers/test.ctrl.js
@@ -0,0 +1,19 @@
+const model = require('../models/main.model');
+
+module.exports = {
+  helloWorld: (req, res) => {
+    res.json({hello: 'world'});
+  },
+
+  testAdd: (req, res) => {
+    const user_name = req.body.name;
+    model.test
+      .create({user_name})
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+};
---
+++ api/routes/test.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const tstCtrl = require('../controllers/test.ctrl');
+
+router
+  .route('/')
+  .get(tstCtrl.helloWorld)
+  .post(tstCtrl.testAdd);
+
+module.exports = router;
