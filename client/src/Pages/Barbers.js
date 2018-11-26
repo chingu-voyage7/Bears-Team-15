d4cafb0a5fa4139c3fb3276352d21969b7f67eb3
Michael Cordero <michaelcordero88@gmail.com>
2018-11-25 18:41:17 -0800

    updated client, Reach Routing and Styled components

---
+++ client/src/Pages/Barbers.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../Components/Common/Layout/index'
+
+
+
+const Barbers = () => (
+	<Layout>
+		<h1>Barbers Page</h1>
+	</Layout>
+)
+
+export default Barbers
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import {IndexPage, Locations, Barbers, About} from './Pages';
+import {Router} from '@reach/router'
+ReactDOM.render(
+<Router>
+<IndexPage path="/" />   
+<Locations path="/locations" />
+<Barbers path="/barbers" /> 
+<About path="/about" />
+</Router> 
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
