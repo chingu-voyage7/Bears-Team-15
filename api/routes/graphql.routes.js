dfe0040b78d89d7e71c7acfa6bf30b44e912879d
aRtoo <raphaelg2012@gmail.com>
2018-11-25 18:18:41 -0800

    created routes for graphql

--- api/routes/graphql.routes.js
+++ api/routes/graphql.routes.js
@@ -1,9 +1,22 @@
 const graphqlHTTP = require('express-graphql');
-const root = require('../controllers/test.graphql');
-const schema = require('../models/test.graphql');
+const {root} = require('../controllers/test.graphql');
+const {helloScheme} = require('../models/test.graphql');
 
 module.exports = graphqlHTTP({
- schema: schema,
+ schema: helloScheme,
  rootValue: root,
  graphiql: true,
 });
+
+// module.exports = graphqlHTTP({
+//  foo: {
+//   schema: helloScheme,
+//   rootValue: root,
+//   graphiql: true,
+//  },
+//  // bar: {
+//  //  schema: testScheme,
+//  //  rootValue: test,
+//  //  graphiql: true,
+//  // },
+// });
