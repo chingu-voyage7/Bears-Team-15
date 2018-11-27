fdfda7853ddc073b0b564a8457a60b2cdee2ad9c
aRtoo <raphaelg2012@gmail.com>
2018-11-26 16:08:10 -0800

    created schema for the users

--- api/graphql_schema/scheme.user.graphql.js
+++ api/graphql_schema/scheme.user.graphql.js
@@ -1,32 +1,74 @@
 const {
  GraphQLObjectType,
  GraphQLString,
- GraphQLInt,
+ GraphQLList,
  GraphQLSchema,
+ GraphQLID,
 } = require('graphql');
 
+const usersCtrl = require('../controllers/users.ctrl');
+
+// this will be the schema of the user
 const UserType = new GraphQLObjectType({
  name: 'User',
  fields: () => ({
-  id: {type: GraphQLInt},
+  id: {type: GraphQLID},
   name: {type: GraphQLString},
+  email: {type: GraphQLString},
+  password: {type: GraphQLString},
+  token: {type: GraphQLString},
  }),
 });
 
 // this will be the query root. to query the user info it should be done with this syntax user(id:){}
-const UserQuery = new GraphQLObjectType({
+const RootQuery = new GraphQLObjectType({
  name: 'RootQuery',
  fields: {
-  user: {
+  userLogin: {
    type: UserType,
-   args: {id: {type: GraphQLInt}},
+   args: {email: {type: GraphQLString}, password: {type: GraphQLString}},
+   resolve(parent, args) {
+    // TODO: query users here!!!!
+    // code to get data in db mongodb query goes here.
+    return usersCtrl
+     .loginUser(args)
+     .then((result) => {
+      // this results return bearer and token
+      return result;
+     })
+     .catch((err) => {});
+   },
+  },
+  userGetAll: {
+   type: new GraphQLList(UserType),
+   // no arguments because where getting every user in the db
+   // args: {id: {type: GraphQLID}},
    resolve(parent, args) {
     // code to get data in db mongodb query goes here.
+    return usersCtrl.getUsers();
+   },
+  },
+ },
+});
+
+const mutateUser = new GraphQLObjectType({
+ name: 'userAdd',
+ // fields property will be the function to add or delete a user
+ fields: {
+  addUser: {
+   type: UserType,
+   args: {
+    name: {type: GraphQLString},
+    email: {type: GraphQLString},
+    password: {type: GraphQLString},
+   },
+   resolve(parent, args) {
+    return usersCtrl.registerUser();
    },
   },
  },
 });
 
 module.exports = new GraphQLSchema({
- query: UserQuery,
+ query: RootQuery,
 });
