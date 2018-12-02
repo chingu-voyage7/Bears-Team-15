657ae37e201e62dd05c401a4a474badd4ef8b1d8
aRtoo <raphaelg2012@gmail.com>
2018-12-01 17:26:21 -0800

    exported functions into main reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux';
 import testReducers from './testReducer';
+import incDecReducers from './inc_dec_reducers';
 
 // pure functions that will be triggered
 export default combineReducers({
   test: testReducers,
+  incDec: incDecReducers,
 });
