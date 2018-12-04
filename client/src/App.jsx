c1da24a298fc12f8dbb45187e391aa1b8197055a
Michael Cordero <michaelcordero88@gmail.com>
2018-12-04 14:43:54 -0800

    moved routes out of layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-
-import Layout from './Components/Common/Layout/index';
-
+import { Router } from '@reach/router';
+import Layout from './Components/Common/Layout/index.js';
+import{Drives, IndexPage, Locations, About, Test} from './Components/Pages/index';
 const App = ({ store }) => (
+  
   <Provider store={store}>
-      <Layout />
+   <Layout>
+     <Router> 
+        <IndexPage path="/" />
+        <Drives path="/drives" />
+        <Locations path="/locations" />
+        <About path="/about" />
+        <Test path="/test" />
+     </Router> 
+   </Layout>
   </Provider>
 );
 
---
+++ client/src/Components/Pages/IndexPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '../Common/Button/button'
+
+
+const IndexPage = () => (
+	<div>
+	<h1>Home Page Content</h1>
+	<Button>this is a styled button</Button>
+	<h1>try /locations /about /drives === What is this line for?</h1>
+</div>
+)
+
+export default IndexPage
