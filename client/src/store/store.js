076e4ec07358d9e0d1dec04c40d67357c1071fd2
aRtoo <raphaelg2012@gmail.com>
2018-12-04 12:46:10 -0800

    change file name

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,15 +1,15 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from '../reducers';
+import rootReducer from '../reducers/rootReducxer';
 
 const middlewares = [thunk];
 const globalInitialState = {
-  incDec: 0
+  incDec: 0,
 };
 
 if (process.env.NODE_ENV !== 'production') {
   // must use 'require' (import only allowed at top of file)
-  const { logger } = require('redux-logger');
+  const {logger} = require('redux-logger');
   middlewares.push(logger);
 }
 // store will will take 3 args
@@ -17,15 +17,17 @@ if (process.env.NODE_ENV !== 'production') {
 // 2nd main state
 // 3rd middleware
 // store is exported to layout index.js
-const configureStore = () => createStore(
-  // either the line above or below
-  // const configureStore = (globalInitialState = {}) => createStore(
-  rootReducer,
-  globalInitialState,
-  compose(
-    applyMiddleware(...middlewares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const configureStore = () =>
+  createStore(
+    // either the line above or below
+    // const configureStore = (globalInitialState = {}) => createStore(
+    rootReducer,
+    globalInitialState,
+    compose(
+      applyMiddleware(...middlewares),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
 
 export default configureStore;
