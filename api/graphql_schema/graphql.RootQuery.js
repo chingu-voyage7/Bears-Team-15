b25f5da5ad65c8b979aa987b54e7a269ef89d0be
aRtoo <raphaelg2012@gmail.com>
2018-12-05 14:53:32 -0800

    separate user query from the root query

---
+++ api/graphql_schema/graphql.RootQuery.js
@@ -0,0 +1,27 @@
+const {GraphQLObjectType, GraphQLSchema} = require('graphql');
+
+const {
+  userGetAll,
+  userLogin,
+  addUser,
+} = require('../graphql_Queries/user.Queries/user.Query.js');
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQuery',
+  fields: {
+    userGetAll,
+    userLogin,
+  },
+});
+
+const mutate = new GraphQLObjectType({
+  name: 'userAdd',
+  fields: {
+    addUser,
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+  mutation: mutate,
+});
