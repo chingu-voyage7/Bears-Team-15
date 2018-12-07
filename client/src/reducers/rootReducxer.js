2076f3a9cfe0b919addd7ecf625cc97a0275b8bd
aRtoo <raphaelg2012@gmail.com>
2018-12-06 17:11:43 -0800

    import graphql and apollo

--- client/src/reducers/rootReducxer.js
+++ client/src/reducers/rootReducxer.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import testReducers from './testReducer';
 import incDecReducers from './inc_dec_reducers';
+import allUsers from './userGrabReducer';
 
 // pure functions that will be triggered
 // combineReducers fn will receive all the reducers
 export default combineReducers({
   test: testReducers,
   incDec: incDecReducers,
+  allUsers: allUsers,
 });
