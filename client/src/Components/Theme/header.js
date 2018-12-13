5df26bd0c5474e86f599ce9b9eb34a18a26ea934
Ujwal Battar <ujwal.battar@gmail.com>
2018-12-13 15:45:34 -0800

    change header to smart component

--- client/src/Components/Theme/header.js
+++ client/src/Components/Theme/header.js
@@ -1,20 +1,42 @@
-import React from 'react'
+import { connect } from 'react-redux';
+import React, { Component } from 'react'
 import { Link } from '@reach/router';
 
-const Header = () => (
-    <div className="header">
-  		<h1 className="logo">ARC</h1>
-      <nav>
-        {/* <Link to="/">Home</Link>{"  "}
-        <Link to="drives">Drives</Link>{"  "}
-        <Link to="locations">Locations</Link>{"  "}
-        <Link to="about">About</Link>{"  "}
-        <Link to="test">Test</Link> */}
-        <Link to="/">Sign Up</Link>{" "}
-        <Link to="/">Login</Link>{" "}
-        <Link to="/">Getting Started</Link>
-      </nav>
-  	</div>
-)
+import { login } from "../../reduxes/actions/loginAction";
+import { openModal } from "../../reduxes/actions/modal_actions.js";
 
-export default Header;
+
+class Header extends Component {
+
+  render() {
+      return (
+        <div className="header">
+      		<h1 className="logo">ARC</h1>
+          <nav>
+            {/* <Link to="/">Home</Link>{"  "}
+            <Link to="drives">Drives</Link>{"  "}
+            <Link to="locations">Locations</Link>{"  "}
+            <Link to="about">About</Link>{"  "}
+            <Link to="test">Test</Link> */}
+            <Link to="/signup">Sign Up</Link>{" "}
+            <span className="login-link" onClick={() => this.props.openModal("login")}>
+              Login
+            </span>{" "}
+            <Link to="/">Getting Started</Link>
+          </nav>
+      	</div>
+      )
+  }
+}
+
+// const mapStateToProps = ({ session }) => ({
+//   // currentUser: session.currentUser
+// });
+
+const mapDispatchToProps = dispatch => ({
+  login: guestUser => dispatch(login(guestUser)),
+  // logout: () => dispatch(logout()),
+  openModal: modal => dispatch(openModal(modal))
+});
+
+export default connect(null, mapDispatchToProps)(Header);
