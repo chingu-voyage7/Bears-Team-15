b20a1c7569992ce0169870b9bf696cbb10a8087a
aRtoo <raphaelg2012@gmail.com>
2018-12-14 16:40:37 -0800

    change the app component to pure component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Provider } from 'react-redux';
+/* eslint-disable no-unused-expressions */
+import React, { Component } from 'react';
+import { Provider, connect } from 'react-redux';
 import { Router } from '@reach/router';
 import Layout from './Components/Common/Layout/index.js';
 import {
@@ -8,30 +9,72 @@ import {
   Locations,
   About,
   Test,
-  Login
+  Login,
+  LoginForm
 } from './Components/Pages/index';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider, compose } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
+// import { clientGraphQL } from './reduxes/actions/clientAction';
 
-const client = new ApolloClient({
-  uri: '/graph',
-});
 
-const App = ({ store }) => (
-  <ApolloProvider client={client}>
-    <Provider store={store}>
-      <Layout>
-        <Router>
-          <IndexPage path="/" />
-          <Drives path="/drives" />
-          <Locations path="/locations" />
-          <About path="/about" />
-          <Test path="/test" />
-          <Login path="/login" client={client} />
-        </Router>
-      </Layout>
-    </Provider>
-  </ApolloProvider>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      client: {}
+    }
+  }
+
+  componentWillMount() {
+    this.setState({
+      client: this.props.store.getState().client
+    })
+  }
+
+  render() {
+    return (
+      <ApolloProvider client={this.state.client}>
+        <Provider store={this.props.store}>
+          <Layout>
+            <Router>
+              <IndexPage path="/" />
+              <Drives path="/drives" />
+              <Locations path="/locations" />
+              <About path="/about" />
+              <Test path="/test" />
+              {/* <LoginForm path="/login" client={client} testing="test" /> */}
+              {/* <Login path="/login" client={client} /> */}
+            </Router>
+          </Layout>
+        </Provider>
+      </ApolloProvider>
+    );
+  }
+}
+
+
+
+// const App = ({ store }) => (
+
+
+//   <ApolloProvider client={store.client}>
+//     <Provider store={store}>
+//       <Layout>
+//         <Router>
+//           <IndexPage path="/" />
+//           <Drives path="/drives" />
+//           <Locations path="/locations" />
+//           <About path="/about" />
+//           <Test path="/test" />
+//           {/* <LoginForm path="/login" client={client} testing="test" /> */}
+//           {/* <Login path="/login" client={client} /> */}
+//         </Router>
+//       </Layout>
+//     </Provider>
+//   </ApolloProvider>
+// )
+
+
 
 export default App;
+
