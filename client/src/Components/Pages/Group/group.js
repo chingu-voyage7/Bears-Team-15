70a7a7f62832933e9e5984a296c88011bcd0fc59
Michael Cordero <michaelcordero88@gmail.com>
2018-12-20 00:07:17 -0800

    event/group pages started

--- client/src/Components/Pages/Group/group.js
+++ client/src/Components/Pages/Group/group.js
@@ -4,7 +4,19 @@ class Group extends React.Component{
 
 render(){
     return(
-        <div>
+        <div className="event-container">
+        <div className="event-title">
+        <h1 >The Gamers Republic</h1>
+        </div> 
+        <img className="event-banner" src={image} alt="event banner"></img>
+            <div className="event-navigation">
+                <h1>Down Town Brunch</h1>
+                <h1>Volunteer</h1>
+            </div>
+            <div className="profile-rule"></div>
+            <div className="Group-content">
+            
+            </div>
             
         </div>
     )
