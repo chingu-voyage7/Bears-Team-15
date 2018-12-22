defffd9ff2e07991a730fbb5741d17f87c7ad47c
Ujwal Battar <ujwal.battar@gmail.com>
2018-12-21 19:34:11 -0800

    change login_reducer to session_reducer and update

--- client/src/reduxes/reducers/session_reducer.js
+++ client/src/reduxes/reducers/session_reducer.js
@@ -1,9 +1,12 @@
 const LOG_IN = 'LOG_IN';
+const SIGN_UP = 'SIGN_UP';
 
 // ! this function should grab token
 export default function (state = {}, action) {
   switch (action.type) {
     case LOG_IN:
+      return action.payload;
+    case SIGN_UP:
       return action.payload;
     default:
       return state;
