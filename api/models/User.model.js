6cc02e34c7ff0d17f9741194a75d1288ea9b9b28
aRtoo <raphaelg2012@gmail.com>
2018-12-31 01:14:46 -0800

    added event property for user

--- api/models/User.model.js
+++ api/models/User.model.js
@@ -4,38 +4,41 @@ const Schema = mongoose.Schema;
 // ! USER SCHEMA HERE
 
 const UserSchema = new Schema({
-  firstName: {
-    type: String,
-    required: [true, 'First name cannot be blank']
-  },
-  lastName: {
-    type: String,
-    required: [true, 'Last name cannot be blank']
-  },
-  email: {
-    type: String,
-    required: [true, 'Email cannot be blank']
-  },
-  password: {
-    type: String,
-    required: [true, 'Password cannot be blank']
-  },
-  age: {
-    type: Number
-  },
-  image: {
-    type: String
-  },
-  phone: {
-    type: String
-  },
-  address: {
-    type: String
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+    firstName: {
+        type: String,
+        required: [true, 'First name cannot be blank'],
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Last name cannot be blank'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Email cannot be blank'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Password cannot be blank'],
+    },
+    age: {
+        type: Number,
+    },
+    image: {
+        type: String,
+    },
+    phone: {
+        type: String,
+    },
+    address: {
+        type: String,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    event: {
+        type: String,
+    },
 });
 
 module.exports = User = mongoose.model('users', UserSchema);
