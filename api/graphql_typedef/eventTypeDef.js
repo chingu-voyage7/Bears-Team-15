094eef70f444f74b121a953d3ff84c8891a526fc
aRtoo <raphaelg2012@gmail.com>
2018-12-31 01:12:32 -0800

    change resolver to func

--- api/graphql_typedef/eventTypeDef.js
+++ api/graphql_typedef/eventTypeDef.js
@@ -12,7 +12,7 @@ const {UserType} = require('./userTypeDef');
 const {getCurrentUser, getUsers} = require('../controllers/users.ctrl');
 
 module.exports = {
-    eventType: new GraphQLObjectType({
+    EventType: new GraphQLObjectType({
         name: 'Event',
         fields: () => ({
             id: {type: GraphQLID},
@@ -27,13 +27,12 @@ module.exports = {
             // ! type relation
             userRelatedToEvent: {
                 type: UserType,
-                async resolve(parent, args) {
+                resolve: async (parent, args) => {
                     const id = {
                         id: parent.organizer,
                     };
                     const test = await getCurrentUser(id);
                     // console.log(Buffer.from(test._id.id).toString());
-                    console.log(test._id);
                     return {...test, ...id};
                 },
             },
