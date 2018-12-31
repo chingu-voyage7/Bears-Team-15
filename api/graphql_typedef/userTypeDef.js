5963286af0c3f4214473afa0880e788c2752e1d0
aRtoo <raphaelg2012@gmail.com>
2018-12-31 01:13:28 -0800

    added a new query and export event ctrls

--- api/graphql_typedef/userTypeDef.js
+++ api/graphql_typedef/userTypeDef.js
@@ -8,6 +8,9 @@ const {
     GraphQLBoolean,
 } = require('graphql');
 
+// const {EventType} = require('./eventTypeDef');
+const {getAllEvents, getEventWithUserId} = require('../controllers/event.ctrl');
+
 module.exports = {
     UserType: new GraphQLObjectType({
         name: 'User',
@@ -22,6 +25,18 @@ module.exports = {
             statusCode: {type: GraphQLInt},
             isSuccess: {type: GraphQLBoolean},
             msg: {type: GraphQLString},
+            event: {type: GraphQLString},
+            eventRelatedToUser: {
+                // TODO: fuck this shit!!!
+                type: require('./eventTypeDef').EventType,
+                resolve: async (parent, args) => {
+                    // console.log(parent.event, 'parent');
+                    const event = {
+                        event: parent.event,
+                    };
+                    return await getEventWithUserId(event);
+                },
+            },
         }),
     }),
 };
