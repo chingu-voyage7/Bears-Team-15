ca3df7b02c2b99ac32af1729b9a70ed9c645245d
aRtoo <raphaelg2012@gmail.com>
2019-01-02 17:28:35 -0800

    added supplies, attendeesId, change object type ID toString

--- api/controllers/event.ctrl.js
+++ api/controllers/event.ctrl.js
@@ -1,23 +1,32 @@
 const Event = require('../models/main.model').event;
 
+// !! This will change OBJECT TYPE TO STRING
+const ObjectId = require('mongoose').Types.ObjectId;
+ObjectId.prototype.valueOf = function() {
+  return this.toString();
+};
+
 module.exports = {
-    getAllEvents: async (data) => {
-        return await Event.find();
-    },
-    // TODO: Fix item here. ask ujwal
-    addEvent: async (data) => {
-        const newEvent = new Event({
-            organizer: data.organizer,
-            title: data.title,
-            date: new Date(),
-            image: data.image,
-            description: data.description,
-            location: data.location,
-            // items: [data.items],
-        });
-        return await newEvent.save();
-    },
-    getEventWithUserId: async (data) => {
-        return await Event.findOne({_id: data.event});
-    },
+  getAllEvents: async (data) => {
+    return await Event.find();
+  },
+  // TODO: Fix item here. ask ujwal
+  addEvent: async (data) => {
+    const newEvent = new Event({
+      organizerId: data.organizerId,
+      title: data.title,
+      date: new Date(),
+      image: data.image,
+      description: data.description,
+      location: data.location,
+      attendeesId: data.attendeesId,
+      supplies: data.supplies
+    });
+
+    console.log(newEvent, 'newEvent');
+    return await newEvent.save();
+  },
+  getEventWithUserId: async (data) => {
+    return await Event.findOne({_id: data.event});
+  }
 };
