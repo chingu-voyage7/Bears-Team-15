daf99f6b9935f0e6bbcf699af075c52e9ac5c1a8
aRtoo <raphaelg2012@gmail.com>
2019-01-02 17:31:05 -0800

    change args names removed commented typedef

--- api/graphql_Resolvers/event.Resolver.js
+++ api/graphql_Resolvers/event.Resolver.js
@@ -1,38 +1,15 @@
 const {
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLList,
-    GraphQLID,
-    GraphQLNonNull,
-    GraphQLInt,
-    GraphQLBoolean,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLID,
+  GraphQLNonNull,
+  GraphQLInt,
+  GraphQLBoolean
 } = require('graphql');
 
 const {EventType} = require('../graphql_typedef/eventTypeDef');
 const eventCtrl = require('../controllers/event.ctrl');
-
-// const eventType = new GraphQLObjectType({
-//     name: 'Event',
-//     fields: () => ({
-//         id: {type: GraphQLID},
-//         organizer: {type: GraphQLString},
-//         title: {type: GraphQLString},
-//         image: {type: GraphQLString},
-//         description: {type: GraphQLString},
-//         location: {type: GraphQLString},
-//         items: {type: GraphQLInt},
-//         date: {type: GraphQLString},
-//         test: {type: GraphQLString},
-//         // ! type relation
-//         userRelatedToEvent: {
-//             type: UserType,
-//             resolve(parent, args) {
-//                 console.log(parent, 'yooow');
-//                 return {id: 'mia khalifa'};
-//             },
-//         },
-//     }),
-// });
 
 // ! testing
 // const {UserType} = require('./user.Resolver');
@@ -40,24 +17,27 @@ const eventCtrl = require('../controllers/event.ctrl');
 // ! RESOLVERS -- THIS WILL BE CALLED ON graphql.RootQuery
 
 module.exports = {
-    getAllEvents: {
-        type: new GraphQLList(EventType),
-        resolve: async (parent, args) => {
-            return await eventCtrl.getAllEvents();
-        },
+  getAllEvents: {
+    type: new GraphQLList(EventType),
+    resolve: async (parent, args) => {
+      return await eventCtrl.getAllEvents();
+    }
+  },
+  addNewEvent: {
+    type: EventType,
+    args: {
+      //   organizerId: {type: GraphQLString},
+      organizerId: {type: new GraphQLList(GraphQLID)},
+      title: {type: GraphQLString},
+      image: {type: GraphQLString},
+      description: {type: GraphQLString},
+      location: {type: GraphQLString},
+      //   attendees: {type: GraphQLString},
+      attendeesId: {type: new GraphQLList(GraphQLID)},
+      supplies: {type: new GraphQLList(GraphQLID)}
     },
-    addNewEvent: {
-        type: EventType,
-        args: {
-            organizer: {type: GraphQLString},
-            title: {type: GraphQLString},
-            image: {type: GraphQLString},
-            description: {type: GraphQLString},
-            location: {type: GraphQLString},
-            // items: {type: GraphQLInt},
-        },
-        resolve: async (parent, args) => {
-            return await eventCtrl.addEvent(args);
-        },
-    },
+    resolve: async (parent, args) => {
+      return await eventCtrl.addEvent(args);
+    }
+  }
 };
