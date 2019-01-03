c98112b6788d532da5ff38c1a2a332ca515d90dd
aRtoo <raphaelg2012@gmail.com>
2019-01-02 16:05:33 -0800

    added new fields

--- api/graphql_typedef/userTypeDef.js
+++ api/graphql_typedef/userTypeDef.js
@@ -1,42 +1,46 @@
 const {
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLList,
-    GraphQLID,
-    GraphQLNonNull,
-    GraphQLInt,
-    GraphQLBoolean,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLID,
+  GraphQLNonNull,
+  GraphQLInt,
+  GraphQLBoolean
 } = require('graphql');
 
 // const {EventType} = require('./eventTypeDef');
 const {getAllEvents, getEventWithUserId} = require('../controllers/event.ctrl');
 
 module.exports = {
-    UserType: new GraphQLObjectType({
-        name: 'User',
-        fields: () => ({
-            id: {type: GraphQLID},
-            firstName: {type: GraphQLString},
-            lastName: {type: GraphQLString},
-            email: {type: GraphQLString},
-            token: {type: GraphQLString},
-            // errors: { type: GraphQLString },
-            test: {type: GraphQLString}, // testing query
-            statusCode: {type: GraphQLInt},
-            isSuccess: {type: GraphQLBoolean},
-            msg: {type: GraphQLString},
-            event: {type: GraphQLString},
-            eventRelatedToUser: {
-                // TODO: fuck this shit!!!
-                type: require('./eventTypeDef').EventType,
-                resolve: async (parent, args) => {
-                    // console.log(parent.event, 'parent');
-                    const event = {
-                        event: parent.event,
-                    };
-                    return await getEventWithUserId(event);
-                },
-            },
-        }),
-    }),
+  UserType: new GraphQLObjectType({
+    name: 'User',
+    fields: () => ({
+      id: {type: GraphQLID},
+      firstName: {type: GraphQLString},
+      lastName: {type: GraphQLString},
+      email: {type: GraphQLString},
+      age: {type: GraphQLInt},
+      token: {type: GraphQLString},
+      image: {type: GraphQLString},
+      phone: {type: GraphQLInt},
+      address: {type: GraphQLString},
+      eventId: {type: GraphQLString}, // <-- this will be the query for userRelatedToUser
+      statusCode: {type: GraphQLInt},
+      isSuccess: {type: GraphQLBoolean},
+      msg: {type: GraphQLString},
+      test: {type: GraphQLString}, // testing query
+      // errors: { type: GraphQ LString },
+      eventRelatedToUser: {
+        // TODO: fuck this shit!!!
+        type: require('./eventTypeDef').EventType,
+        resolve: async (parent, args) => {
+          // console.log(parent.event, 'parent');
+          const event = {
+            event: parent.eventId
+          };
+          return await getEventWithUserId(event);
+        }
+      }
+    })
+  })
 };
