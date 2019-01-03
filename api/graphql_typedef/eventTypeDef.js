5d5d7e7d9c7d25551239fd9592f9e208218cea56
aRtoo <raphaelg2012@gmail.com>
2019-01-02 17:33:26 -0800

    change/added new fields type and prop

--- api/graphql_typedef/eventTypeDef.js
+++ api/graphql_typedef/eventTypeDef.js
@@ -1,41 +1,43 @@
 const {
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLList,
-    GraphQLID,
-    GraphQLNonNull,
-    GraphQLInt,
-    GraphQLBoolean,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLID,
+  GraphQLNonNull,
+  GraphQLInt,
+  GraphQLBoolean
 } = require('graphql');
 
 const {UserType} = require('./userTypeDef');
 const {getCurrentUser, getUsers} = require('../controllers/users.ctrl');
 
 module.exports = {
-    EventType: new GraphQLObjectType({
-        name: 'Event',
-        fields: () => ({
-            id: {type: GraphQLID},
-            organizer: {type: GraphQLString},
-            title: {type: GraphQLString},
-            image: {type: GraphQLString},
-            description: {type: GraphQLString},
-            location: {type: GraphQLString},
-            items: {type: GraphQLInt},
-            date: {type: GraphQLString},
-            test: {type: GraphQLString},
-            // ! type relation
-            userRelatedToEvent: {
-                type: UserType,
-                resolve: async (parent, args) => {
-                    const id = {
-                        id: parent.organizer,
-                    };
-                    const test = await getCurrentUser(id);
-                    // console.log(Buffer.from(test._id.id).toString());
-                    return {...test, ...id};
-                },
-            },
-        }),
-    }),
+  EventType: new GraphQLObjectType({
+    name: 'Event',
+    fields: () => ({
+      id: {type: GraphQLID},
+      organizerId: {type: new GraphQLList(GraphQLID)},
+      title: {type: GraphQLString},
+      image: {type: GraphQLString},
+      description: {type: GraphQLString},
+      location: {type: GraphQLString},
+      items: {type: GraphQLInt},
+      date: {type: GraphQLString},
+      attendeesId: {type: new GraphQLList(GraphQLID)},
+      supplies: {type: new GraphQLList(GraphQLID)},
+      test: {type: GraphQLString},
+      // ! type relation
+      userRelatedToEvent: {
+        type: UserType,
+        resolve: async (parent, args) => {
+          const id = {
+            id: parent.organizer
+          };
+          const test = await getCurrentUser(id);
+          // console.log(Buffer.from(test._id.id).toString());
+          return {...test, ...id};
+        }
+      }
+    })
+  })
 };
