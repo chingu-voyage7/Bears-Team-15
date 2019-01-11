213a49066978d27073b686124c0b8b637251ae94
aRtoo <raphaelg2012@gmail.com>
2019-01-10 17:46:55 -0800

    added filter events

--- api/controllers/event.ctrl.js
+++ api/controllers/event.ctrl.js
@@ -1,32 +1,38 @@
 const Event = require('../models/main.model').event;
+const User = require('../models/main.model').user;
 
 // !! This will change OBJECT TYPE TO STRING
 const ObjectId = require('mongoose').Types.ObjectId;
 ObjectId.prototype.valueOf = function() {
-  return this.toString();
+    return this.toString();
 };
 
 module.exports = {
-  getAllEvents: async (data) => {
-    return await Event.find().populate('attendees organizer');
-  },
-  // TODO: Fix item here. ask ujwal
-  addEvent: async (data) => {
-    const newEvent = new Event({
-      organizer: data.organizer,
-      title: data.title,
-      date: new Date(),
-      image: data.image,
-      description: data.description,
-      location: data.location,
-      attendees: data.attendees,
-      supplies: data.supplies
-    });
+    getAllEvents: async (data) => {
+        return await Event.find().populate('attendees organizer');
+    },
+    // TODO: Fix item here. ask ujwal
+    addEvent: async (data) => {
+        const newEvent = new Event({
+            organizer: data.organizer,
+            title: data.title,
+            date: new Date(),
+            image: data.image,
+            description: data.description,
+            location: data.location,
+            attendees: data.attendees,
+            supplies: data.supplies,
+        });
 
-    console.log(newEvent, 'newEvent');
-    return await newEvent.save();
-  },
-  getEventWithEventId: async (data) => {
-    return await Event.findOne({_id: data.event});
-  }
+        console.log(newEvent, 'newEvent');
+        return await newEvent.save();
+    },
+    getEventWithEventId: async (data) => {
+        return await Event.findOne({_id: data.event});
+    },
+    filteredEventWith: async (data) => {
+        return await Event.find({
+            title: {$regex: '.*' + data.char + '.*', $options: 'i'},
+        }).populate('attendees organizer');
+    },
 };
