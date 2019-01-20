6f4be2b1fb0e7b0459452e92280adb0069bad840
aRtoo <raphaelg2012@gmail.com>
2019-01-19 17:05:11 -0800

    created a state of object

--- client/src/reduxes/actions/modal_actions.js
+++ client/src/reduxes/actions/modal_actions.js
@@ -1,15 +1,19 @@
-export const OPEN_MODAL = "OPEN_MODAL";
-export const CLOSE_MODAL = "CLOSE_MODAL";
+export const OPEN_MODAL = 'OPEN_MODAL';
+export const CLOSE_MODAL = 'CLOSE_MODAL';
 
-export const openModal = modal => {
-  return {
-    type: OPEN_MODAL,
-    modal
-  };
+export const openModal = (modal, data) => {
+    console.log(modal, data);
+    return {
+        type: OPEN_MODAL,
+        payload: {
+            modal,
+            data,
+        },
+    };
 };
 
 export const closeModal = () => {
-  return {
-    type: CLOSE_MODAL
-  };
+    return {
+        type: CLOSE_MODAL,
+    };
 };
