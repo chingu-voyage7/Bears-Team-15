0977324cecf36b79d64a292305b3f1f32882cb69
aRtoo <raphaelg2012@gmail.com>
2019-01-22 19:31:07 -0800

    added setCurrentState to grab user info on new tab if user is still authenticated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,143 +1,145 @@
 /* eslint-disable no-unused-expressions */
-import React, { Component } from 'react';
-import { graphql, compose } from 'react-apollo';
-import { Provider, connect } from 'react-redux';
-import { Router } from '@reach/router';
+import React, {Component} from 'react';
+import {graphql, compose} from 'react-apollo';
+import {Provider, connect} from 'react-redux';
+import {Router} from '@reach/router';
 import Layout from './Components/Common/Layout/index.js';
 import {
-  Drives,
-  IndexPage,
-  About,
-  Test,
-  Search,
-  Profile,
-  Event,
-  NotFound,
-  // Login,
-  LoginForm
+    Drives,
+    IndexPage,
+    About,
+    Test,
+    Search,
+    Profile,
+    Event,
+    NotFound,
+    // Login,
+    LoginForm,
 } from './Components/Pages/index';
-import { ApolloProvider } from 'react-apollo';
+import {ApolloProvider} from 'react-apollo';
 import jwtDecode from 'jwt-decode';
 
 // ! helpers
-import SetGetCookie from './util/helper.cookie'
+import SetGetCookie from './util/helper.cookie';
 
 // ! components
-import Auth from './Components/Common/Auth/Auth'
+import Auth from './Components/Common/Auth/Auth';
 
 // ! action
-import { auth } from './reduxes/actions/isAuthAction.js';
-import { closeModal } from './reduxes/actions/modal_actions';
+import {auth} from './reduxes/actions/isAuthAction.js';
+import {closeModal} from './reduxes/actions/modal_actions';
 import ApolloClient from 'apollo-boost';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { currUser } from './util/graphQLQuery';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {HttpLink} from 'apollo-link-http';
+import {setCurrentUser} from './reduxes/actions/session_actions';
+
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({
-  uri: '/graph',
-  link: new HttpLink(),
-  cache
+    uri: '/graph',
+    link: new HttpLink(),
+    cache,
 });
 // ! query
 
-
-const { getCookie } = new SetGetCookie('tokenizer');
+const {getCookie} = new SetGetCookie('tokenizer');
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     client: {},
-  //   }
-  // }
-
-  componentWillMount() {
-    // this.setState({
-    //   client: this.props.store.getState().client
-    // })
-  }
-
-  componentDidMount() {
-
-    // TODO: make a condition that checks the token.
-    // ! if token returns true then query, if not then let user login
-    // const { client } = this.props.state;
-    // takes hashtoken from cookie
-    const hashToken = getCookie();
-
-    // checker if the theres a token in the cookie
-    if (hashToken === '0' || !hashToken) {
-      return
-    }
-    this.props.auth(true)
-    this.props.closeModal();
-    // decode the token and grab the ID
-    // const { id } = jwtDecode(hashToken)
-    // // query the user, takes id as a variables
-    // const userWhoIsLoggedIn = await client.query({
-    //   query: currUser,
-    //   variables: {
-    //     id: id,
+    // constructor(props) {
+    //   super(props);
+    //   this.state = {
+    //     client: {},
     //   }
-    // });
+    // }
 
-    // const { email, isSuccess } = userWhoIsLoggedIn.data.currentUser;
-    // pass the email and isSuccess as an argument
-    // this.handleUserCheckIfLoggedI(email, isSuccess);
-  }
+    componentWillMount() {
+        // this.setState({
+        //   client: this.props.store.getState().client
+        // })
+    }
 
-  /**
+    componentDidMount() {
+        // TODO: make a condition that checks the token.
+        // ! if token returns true then query, if not then let user login
+        // const { client } = this.props.state;
+        // takes hashtoken from cookie
+        const hashToken = getCookie();
+
+        // checker if the theres a token in the cookie
+        if (hashToken === '0' || !hashToken) {
+            return;
+        }
+        this.props.auth(true);
+        this.props.setCurrUser(jwtDecode(hashToken));
+        this.props.closeModal();
+        // decode the token and grab the ID
+        // const { id } = jwtDecode(hashToken)
+        // // query the user, takes id as a variables
+        // const userWhoIsLoggedIn = await client.query({
+        //   query: currUser,
+        //   variables: {
+        //     id: id,
+        //   }
+        // });
+
+        // const { email, isSuccess } = userWhoIsLoggedIn.data.currentUser;
+        // pass the email and isSuccess as an argument
+        // this.handleUserCheckIfLoggedI(email, isSuccess);
+    }
+
+    /**
   //  * takes an string and bool
   //  * @param {STRING} email
   //  * @param {BOOL} stat
   //  */
-  // handleUserCheckIfLoggedI = (email, stat) => {
-  //   const { closeModal, auth } = this.props;
-  //   if (email && stat) {
-  //     auth(true)
-  //     closeModal();
-  //     console.log('success');
-  //   } else {
-  //     console.log('denied');
-  //     auth(false);
-  //     // TODO: call open modal here
-  //   }
-  // }
+    // handleUserCheckIfLoggedI = (email, stat) => {
+    //   const { closeModal, auth } = this.props;
+    //   if (email && stat) {
+    //     auth(true)
+    //     closeModal();
+    //     console.log('success');
+    //   } else {
+    //     console.log('denied');
+    //     auth(false);
+    //     // TODO: call open modal here
+    //   }
+    // }
 
-
-  render() {
-
-    return (
-      <ApolloProvider client={client}>
-        <Provider store={this.props.store}>
-          <Layout>
-            <Router>
-            <NotFound default />
-              <IndexPage path="/" />
-              <Drives path="/drives" />
-              {/* <Locations path="/locations" /> */}
-              <LoginForm path="/login" />
-              <Auth path="/test" component={Test} />
-              <Auth path="/about" component={About} />
-              <Search path="/search" />
-              <Auth path="/profile" component={Profile}/>
-              <Event path="/event/:EventId" />
-            </Router>
-          </Layout>
-        </Provider>
-      </ApolloProvider>
-    );
-  }
+    render() {
+        return (
+            <ApolloProvider client={client}>
+                <Provider store={this.props.store}>
+                    <Layout>
+                        <Router>
+                            <NotFound default />
+                            <IndexPage path="/" />
+                            <Drives path="/drives" />
+                            {/* <Locations path="/locations" /> */}
+                            <LoginForm path="/login" />
+                            <Auth path="/test" component={Test} />
+                            <Auth path="/about" component={About} />
+                            <Search path="/search" />
+                            <Auth path="/profile" component={Profile} />
+                            <Event path="/event/:EventId" />
+                        </Router>
+                    </Layout>
+                </Provider>
+            </ApolloProvider>
+        );
+    }
 }
 
 const mapStateToProps = (state) => ({
-  state
+    state,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  auth: (args) => dispatch(auth(args)),
-  closeModal: () => dispatch(closeModal())
-})
+    auth: (args) => dispatch(auth(args)),
+    closeModal: () => dispatch(closeModal()),
+    setCurrUser: (args) => dispatch(setCurrentUser(args)),
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
