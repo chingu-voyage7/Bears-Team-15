610f5a0a07f848ceb9b5d5cb46e99a40de20ca7b
aRtoo <raphaelg2012@gmail.com>
2019-01-22 19:33:00 -0800

    fixed auth, setting token name

--- client/src/Components/Common/Auth/Auth.jsx
+++ client/src/Components/Common/Auth/Auth.jsx
@@ -1,64 +1,57 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 // import { Redirect } from '@reach/router';
-import { compose } from 'react-apollo';
-import { connect } from 'react-redux';
-import { openModal } from '../../../reduxes/actions/modal_actions';
+import {compose} from 'react-apollo';
+import {connect} from 'react-redux';
+import {openModal} from '../../../reduxes/actions/modal_actions';
 import jwtDecode from 'jwt-decode';
 import {setCurrentUser} from '../../../reduxes/actions/session_actions';
-import { set } from "mongoose";
-// import SetGetCookie from '../../util/helper.cookie';
+import {set} from 'mongoose';
+import SetGetCookie from '../../../util/helper.cookie';
 
 class Auth extends Component {
+    checkAuthenticate = () => {
+        const {component: Component, path, isAuth} = this.props;
 
- checkAuthenticate = () => {
-  const { component: Component, path, isAuth } = this.props;
+        console.log(isAuth);
+        if (isAuth) {
+            const {getCookie} = new SetGetCookie('tokenizer');
 
-  if (isAuth) {
-    // this.props.setCurrentUser();
-    console.log('currentUser',this.props.currentUser);
-    if(!this.props.currentUser.id){
-      this.props.setCurrentUser(jwtDecode(window.document.cookie));
+            const hashToken = getCookie();
+            if (!this.props.currentUser.id) {
+                this.props.setCurrentUser(jwtDecode(hashToken));
+            }
+
+            return <Component path={path} />;
+        } else {
+            // return (
+            //  <Redirect from={path} to='/login' noThrow />
+            // )
+            this.props.openModal('LOGIN');
+        }
+    };
+
+    render() {
+        return <>{this.checkAuthenticate()}</>;
     }
-    
-   return (
-    <Component path={path} />
-   )
-  } else {
-   // return (
-   //  <Redirect from={path} to='/login' noThrow />
-   // )
-   this.props.openModal('login');
-  }
- }
-
- render() {
-  return (
-   <>
-    {
-     this.checkAuthenticate()
-    }
-   </>
-  )
- }
 }
 
 const mapStateToProps = (state) => {
- return {
-  isAuth: state.isAuth,
-  currentUser: state.currentUser
- };
+    return {
+        isAuth: state.isAuth,
+        currentUser: state.currentUser,
+    };
 };
 
 const mapDispatchToProps = (dispatch) => {
- return {
-  openModal: (args) => dispatch(openModal(args)),
-  setCurrentUser: (args)=> dispatch(setCurrentUser(args))
- }
+    return {
+        openModal: (args) => dispatch(openModal(args)),
+        setCurrentUser: (args) => dispatch(setCurrentUser(args)),
+    };
 };
 
 export default compose(
- connect(
-  mapStateToProps,
-  mapDispatchToProps
- ),
+    connect(
+        mapStateToProps,
+        mapDispatchToProps
+    )
 )(Auth);
