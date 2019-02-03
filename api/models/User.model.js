16a4ff1bb5b234035c8fdd8ea8c7e305dfed4cb5
aRtoo <raphaelg2012@gmail.com>
2019-02-02 22:15:52 -0800

    added attendedEvent in user model

--- api/models/User.model.js
+++ api/models/User.model.js
@@ -4,42 +4,40 @@ const Schema = mongoose.Schema;
 // ! USER SCHEMA HERE
 
 const UserSchema = new Schema({
-  firstName: {
-    type: String,
-    required: [true, 'First name cannot be blank']
-  },
-  lastName: {
-    type: String,
-    required: [true, 'Last name cannot be blank']
-  },
-  email: {
-    type: String,
-    required: [true, 'Email cannot be blank']
-  },
-  password: {
-    type: String,
-    required: [true, 'Password cannot be blank']
-  },
-  age: {
-    type: Number
-  },
-  image: {
-    type: String
-  },
-  phone: {
-    type: Number
-  },
-  address: {
-    type: String
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  },
-  eventsId: [{type: Schema.Types.ObjectId, ref: 'events'}]
-  //   event: {
-  //     type: String
-  //   },
+    firstName: {
+        type: String,
+        required: [true, 'First name cannot be blank'],
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Last name cannot be blank'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Email cannot be blank'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Password cannot be blank'],
+    },
+    age: {
+        type: Number,
+    },
+    image: {
+        type: String,
+    },
+    phone: {
+        type: Number,
+    },
+    address: {
+        type: String,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    attendedEvent: [{type: Schema.Types.ObjectId, ref: 'events'}],
+    eventsId: [{type: Schema.Types.ObjectId, ref: 'events'}],
 });
 
 module.exports = User = mongoose.model('users', UserSchema);
