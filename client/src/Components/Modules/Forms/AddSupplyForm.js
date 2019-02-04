f65ff8d5a0cf8199e309ef730f3ac1922307d004
cyberputty <michaelcordero88@gmail.com>
2019-02-03 18:27:39 -0800

    added supply forms

---
+++ client/src/Components/Modules/Forms/AddSupplyForm.js
@@ -0,0 +1,69 @@
+import React from "react"
+import { connect } from "react-redux"
+import { graphql, compose } from 'react-apollo';
+import {getUser, getEventById} from '../../../util/graphQLQuery';
+import "./forms.scss"
+import {closeModal} from "../../../reduxes/actions/modal_actions";
+const AddSupplyForm = ({ client, currentUser,data}) => {
+    let form = {
+        eventId:data.eventId,
+        name: '',
+        description: currentUser.id,
+        quantity: ''
+    }
+    
+    const onChange = (event) => {
+        console.log('name', event.target.name);
+        console.log('input', event.target.value);
+        console.log('frommodal',data);
+        form[event.target.name] = event.target.value;
+        console.log(form);
+
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+        client.mutate({}).then((data) => {
+            closeModal();
+           
+        });
+
+    }
+    return (<div className="modal-form">
+        <form className="modal-event" onSubmit={onSubmit}>
+            <h2>Edit Profile</h2>
+            <div className="modal-event-field"><label>Name</label><input type="text" name="firstName" onChange={onChange} placeholder={data.firstName}/></div>
+            <div className="modal-event-field"><label>Avatar</label><input type="url" name="image" onChange={onChange} placeholder={data.image}/></div>
+            <div className="modal-event-field"><label>Email</label><input type="email" name="email" onChange={onChange} placeholder={data.email}/></div>
+            <div className="modal-event-field"><label>Phone</label><input type="text" name="phone" onChange={onChange} placeholder={data.phone}/></div>
+            {/*public or private needs field */}
+            <button>Submit</button>
+        </form>
+    </div>);
+}
+
+
+const mapStateToProps = (state) => ({
+    // get supplies list
+    currentUser: state.currentUser,
+    client: state.client,
+    data: state.modal.data
+});
+const mapDispatchToProps = (dispatch) => ({
+    closeModal: ()=>{dispatch(closeModal());}
+});
+
+
+export default compose(connect(
+  mapStateToProps,
+  mapDispatchToProps
+  // replace with edit user
+), graphql(getUser, {
+  name: "getUser", options: (props) => {
+      console.log("graphprops", props)
+      return {
+          variables: {
+              id: props.currentUser.id
+          }
+      }
+  }
+}))(AddSupplyForm);
--- client/src/Components/Pages/Event/event.js
+++ client/src/Components/Pages/Event/event.js
@@ -163,7 +163,7 @@ class Event extends React.Component {
                                 <img src={exclamation} alt="exclamation mark" />{' '}
                                 needs supplies
                             </h2> 
-                            <button>add supplies</button>
+                            <button onClick={()=>this.props.openModal('ADD_SUPPLY')}>add supplies</button>
                             <ul className="event-supply-list">
                             {supplies.map((supply) => {
                                 let total= 0;
